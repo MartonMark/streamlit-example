7f87880af20186ef281c3091726f999f6bacde40
MartonMark <90178816+MartonMark@users.noreply.github.com>
2023-11-23 13:50:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("Calculator App using Streamlit")
-st.write("---")
+st.write("xdddddddddddddddddddd")
 
 num1 = st.number_input(label="Enter first number")
 num2 = st.number_input(label="Enter second number")
