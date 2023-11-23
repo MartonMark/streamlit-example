52ec6b82ad53605bc9e74185e273e95d8dc7a95b
MartonMark <90178816+MartonMark@users.noreply.github.com>
2023-11-23 14:22:47 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.title("számológép")
 st.write("---")
 
-number1 = st.number_input(label = "")
+number1 = st.number_input()
 
-number2 = st.number_input(label = "")
+number2 = st.number_input()
 
 
 
