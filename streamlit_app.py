ee42a00a20484bc9d5b9003ab2d255a426013505
MartonMark <90178816+MartonMark@users.noreply.github.com>
2023-11-23 14:26:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,9 @@ st.title("számológép")
 st.write("---")
 
 number1 = st.number_input(label = "szam 1")
-
 number2 = st.number_input(label = "szam 2")
 
-st.write("válasszd ki mit akarsz te csicska?")
+st.write("anyukad szuz?")
 
 
 
