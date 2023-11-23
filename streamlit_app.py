7061ad29a4b3fe5a44ddfad1450f06bdaab91d1c
MartonMark <90178816+MartonMark@users.noreply.github.com>
2023-11-23 09:08:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ st.write("---")
 
 num1 = st.number_input(label="Enter first number")
 num2 = st.number_input(label="Enter second number")
+st.write("Operation")
+
+operation = st.radio("Select an operation to perform:",("Add", "Subtract","Multiply", "Divide"))
