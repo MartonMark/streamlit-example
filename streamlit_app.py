654423075ab2aaf8124a8ba70a9546fb5f5dcfbf
MartonMark <90178816+MartonMark@users.noreply.github.com>
2023-11-23 09:19:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,19 @@ num2 = st.number_input(label="Enter second number")
 st.write("Operation")
  
 operation = st.radio("Select an operation to perform:",("összebaszás", "kibaszás", "szorbaszás", "basztás"))
+
+ans = 0
+def calculate():
+ if operation == "összebaszás":
+  ans = num1 + num2
+ elif operation == "kibaszás":
+  ans = num1 - num2
+ elif operation == "szorbaszás":
+  ans = num1 * num2
+ elif operation == "divide" and num2!=0:
+  ans = num1 / num2
+ else:
+  st.warning("nem lehet nullával osztani te autista kutya")
+  ans = "nincs értelmezve"
+
+st.success(f"Answer = {ans}")
