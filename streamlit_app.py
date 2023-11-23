57ba7bb9705e43f6f4afbf151677cbf61dd9a0b3
MartonMark <90178816+MartonMark@users.noreply.github.com>
2023-11-23 14:41:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,28 @@ number2 = st.number_input(label = "szam 2")
 
 st.write("anyukad szuz?")
 st.radio("muveletek",("osszeadas", "kivonas", "szorzas", "osztas"))
+
+ans = 0
+
+def calculator():
+  if muveletek == "osszeadas":
+    ans = number1 + number2
+  elif muveletek == "kivonas":
+    ans = number1 - number2
+  elif muveletek == "szorzas":
+    ans = number1 * number2
+  elif muveletek == "osztas" and number2!=0:
+    ans = number1 / number2 
+  else:
+    st.warning("XDDDDD annyira siralmas vagy nemtudok mit mondani"):
+    ans = "szar"
+
+
+st.succes(f"valasz = {ans}")
+
+if st.button("kalkulalas"):
+  calculate()
+    
+    
+
+
