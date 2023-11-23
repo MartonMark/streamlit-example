052039011231b0f6e046ecc0d94ea1d30ba92f7b
MartonMark <90178816+MartonMark@users.noreply.github.com>
2023-11-23 09:32:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 st.title("Calculator App using Streamlit")
-
 st.write("---")
 
 num1 = st.number_input(label="Enter first number")
@@ -11,6 +10,7 @@ st.write("Operation")
 operation = st.radio("Select an operation to perform:",("összebaszás", "kibaszás", "szorbaszás", "basztás"))
 
 ans = 0
+
 def calculate():
  if operation == "összebaszás":
   ans = num1 + num2
