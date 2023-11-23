a9cba27f8aaf0687b60b41f89a09dde5fc6074d8
MartonMark <90178816+MartonMark@users.noreply.github.com>
2023-11-23 14:29:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ number1 = st.number_input(label = "szam 1")
 number2 = st.number_input(label = "szam 2")
 
 st.write("anyukad szuz?")
-st.radio("müveletek",("osszeadas", "kivonas", "szorzas", "osztas")
+st.radio("muveletek",("osszeadas", "kivonas", "szorzas", "osztas")
 
 
 
