1d9ffa210f3b912a4c615349eeebcc4881ce5963
MartonMark <90178816+MartonMark@users.noreply.github.com>
2023-11-29 14:08:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,29 +3,29 @@ import streamlit as st
 st.title("szamologep")
 st.write("---")
 
-number1 = st.number_input(label = "szam 1")
+numuber1 = st.nmber_input(label = "szam 1")
 number2 = st.number_input(label = "szam 2")
 
-st.write("muveletek")
-muveletek = st.radio("muveletek",("osszeadas", "kivonas", "szorzas", "osztas"))
+st.write("operation")
+operation = st.radio("muveletek",("osszeadas", "kivonas", "szorzas", "osztas"))
 
 ans = 0
 
 def calculate():
-  if muveletek == "osszeadas":
+  if operation == "osszeadas":
     ans = number1 + number2
-  elif muveletek == "kivonas":
+  elif operation == "kivonas":
     ans = number1 - number2
-  elif muveletek == "szorzas":
+  elif operation == "szorzas":
     ans = number1 * number2
-  elif muveletek == "osztas" and number2!=0:
+  elif operation == "osztas" and number2!=0:
     ans = number1 / number2 
   else:
     st.warning("XDDDDD annyira siralmas vagy nemtudok mit mondani")
     ans = "szar"
 
 
-  st.succes(f"valasz = {ans}")
+  st.success(f"Answer = {ans}")
 
 if st.button("calculator"):
   calculate()
