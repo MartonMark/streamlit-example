0a1b920ee626313077b26ac4a95afb4cdb77db0a
MartonMark <90178816+MartonMark@users.noreply.github.com>
2023-12-07 08:30:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,14 +39,7 @@ xdd = st.slider("valassz mennyi kukit szeretnel a seggede", 0,10)
 st.write(xdd**2)
 
 
-with st.form("my_form"):
-   st.write("Inside the form")
-   slider_val = st.slider("Form slider")
-   checkbox_val = st.checkbox("Form checkbox")
+import streamlit as st
 
-   # Every form must have a submit button.
-   submitted = st.form_submit_button("Submit")
-   if submitted:
-       st.write("slider", slider_val, "checkbox", checkbox_val)
-
-st.write("Outside the form")
+title = st.text_input('add ma meg az emailod legyszike')
+st.write('koszike', title)
