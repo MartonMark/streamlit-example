ce5a9e2b1d8ef96e8d4c482166a8f64a374dd8ab
MartonMark <90178816+MartonMark@users.noreply.github.com>
2023-12-07 08:21:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,3 +37,16 @@ if st.button("calculator"):
 
 xdd = st.slider("valassz mennyi kukit szeretnel a seggede", 0,10)
 st.write(xdd**2)
+
+
+with st.form("my_form"):
+   st.write("Inside the form")
+   slider_val = st.slider("Form slider")
+   checkbox_val = st.checkbox("Form checkbox")
+
+   # Every form must have a submit button.
+   submitted = st.form_submit_button("Submit")
+   if submitted:
+       st.write("slider", slider_val, "checkbox", checkbox_val)
+
+st.write("Outside the form")
