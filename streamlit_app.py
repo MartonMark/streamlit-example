de9a88c3a0d529ede5c0df754467a0c97273dca7
MartonMark <90178816+MartonMark@users.noreply.github.com>
2023-12-07 08:45:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,10 +31,6 @@ if st.button("calculator"):
   calculate()
 
 
-option = st.selectbox(
-    'nemed',
-    ('Ferfi', 'No'))
-
 
 
 
@@ -42,11 +38,35 @@ xdd = st.slider("valassz mennyi kukit szeretnel a seggede", 0,10)
 st.write(xdd**2)
 
 
-import streamlit as st
+def main():
+    st.title("E-mail Bekérés")
 
-email = st.text_input('add ma meg az emailod legyszike')
-nev = st.text_input('add ma meg az neved legyszike')
-xddddfdddddd = st.slider("eletkorod", 1,100)
+    # Felhasználó nevének bekérése
+    name = st.text_input("Kérlek, add meg a neved:")
 
+    # E-mail cím bekérése
+    email = st.text_input("Kérlek, add meg az e-mail címed:")
+
+    age = st.number_input("Kérlek, add meg az életkorod:", min_value=0, max_value=300)    
+
+    gender = st.selectbox(
+    'nemed',
+    ('Ferfi', 'No', 'slampos'))
+
+    # Elküld gomb
+    if st.button("Elküld"):
+        if name and email:
+            save_to_file(name, email)
+            st.success(f"Köszönjük, {name}! Az e-mail címed: {email}. Az adatokat elmentettük.")
+        else:
+            st.error("Kérlek, töltsd ki mindkét mezőt!")
+                
+
+def save_to_file(name, email,age):
+    with open("adatok.txt", "a") as file:
+        file.write(f"Név: {name}, E-mail: {email}\n,{age},{gender} ")
+
+if __name__ == "__main__":
+    main()
 
 
