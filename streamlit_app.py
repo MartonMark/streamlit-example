dc87ae4b97d0d155bb53937d9bea7b6e2077e598
MartonMark <90178816+MartonMark@users.noreply.github.com>
2023-12-07 09:29:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,40 @@
-git add adatok.txt
+import sqlite3
+
+def save_to_database(name, email, age, gender):
+    # Adatbázis kapcsolat létrehozása (ha még nem létezik, létrehozza)
+    conn = sqlite3.connect("adatok.db")
+
+    # Cursor létrehozása
+    cursor = conn.cursor()
+
+    # Tábla létrehozása, ha még nem létezik
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS felhasznalok (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nev TEXT,
+            email TEXT,
+            eletkor INTEGER,
+            nem TEXT
+        )
+    ''')
+
+    # Adatok beszúrása
+    cursor.execute('''
+        INSERT INTO felhasznalok (nev, email, eletkor, nem)
+        VALUES (?, ?, ?, ?)
+    ''', (name, email, age, gender))
+
+    # Tranzakció lezárása és mentése
+    conn.commit()
+
+    # Kapcsolat lezárása
+    conn.close()
+
+# Teszt adatok
+nev = "John Doe"
+email = "john.doe@example.com"
+eletkor = 25
+nem = "Ferfi"
+
+# Adatok beszúrása az adatbázisba
+save_to_database(nev, email, eletkor, nem)
